626f432747fee1c5399ace00e05e9744862538ca
EdnaLira <ada_hack548@womenintech.mx>
2023-12-02 03:20:18 -0600

    Algoritmo para obtener el área de un triángulo por JS

---
+++ Área_triángulo.js
@@ -0,0 +1,9 @@
+//Definiendo datos
+let base = prompt ("Para cualcular el área de tu triángulo, ingresa la medida de la base en cm:");
+let altura = prompt ("Ahora ingresa la medida de la altura en cm:");
+
+//Cálculo
+let area = (base*altura)/2
+
+//Impresión de resultado
+console.log("El área de tu triángulo es de " + area + " cm2.")
